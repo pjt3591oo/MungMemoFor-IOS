52dbf8428ab4c9f77700603d6b7f03b1a5f3f6db
pjt3591oo <pjt3591oo@gmail.com>
2019-12-14 15:27:23 +0900

    slash 화면 설정과 모델 그룹을 생성하여 memo 모델을 추가한다 slash 화면을 설정했지만 동작을 안하는 것 같음

---
+++ MungMEmo/Model/Model.swift
@@ -0,0 +1,26 @@
+//
+//  Model.swift
+//  MungMEmo
+//
+//  Created by 박정태 on 2019/12/14.
+//  Copyright © 2019 JeongTae Park. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init (content: String) {
+        self.content = content
+        self.insertDate = Date()
+    }
+    
+    static var dumyMemoList = [
+        Memo(content: "mung1"),
+        Memo(content: "mung2"),
+        Memo(content: "mung3"),
+        Memo(content: "mung4")
+    ]
+}
